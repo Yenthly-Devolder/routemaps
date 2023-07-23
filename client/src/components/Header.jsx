b001971e9803dcb0928be3963fd6c43bd2f6d54c
Yenthly-Devolder <yenthly.devolder@student.hogent.be>
2023-07-23 19:49:55 +0200

    init post request

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,24 +1,33 @@
-import Navbar from './Navbar'
+import Navbar from "./Navbar";
+import axios from "axios";
+
+console.log(axios.isCancel("something"));
 export default function Header() {
   function addDropOff(address) {
-    
+    axios.post("https://api.mapbox.com/optimized-trips/v2", {
+      version: 1,
+      vehicles: [],
+      services: [],
+    });
   }
 
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <p>Header</p>
       <label htmlFor="Dropoff address">Dropoff address</label>
       <input type="text" />
-      <input type="button" value="addDropOff" id="addDropOff" onClick={
-        addDropOff
-      }/>
+      <input
+        type="button"
+        value="addDropOff"
+        id="addDropOff"
+        onClick={addDropOff}
+      />
       <input type="text" />
-      <input type="button" value="addDropOff" id="addDropOff" />np
+      <input type="button" value="addDropOff" id="addDropOff" />
+      np
     </div>
   );
-}   
+}
 
-<style>
-
-</style>
+<style></style>;
